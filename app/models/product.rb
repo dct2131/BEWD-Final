e75053d59b31bdc92a4e9fa7ca50199aef492a9e
David <dav2724@gmail.com>
2015-05-18 21:36:47 -0400

    added bootstrap and devise

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,3 @@
 class Product < ActiveRecord::Base
-  has_many :projects
+  has_and_belongs_to_many :projects
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
 
+  devise_for :users
   root 'projects#index'
 
   resources :projects
   resources :products
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
