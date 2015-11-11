85e774e77fc6aec6b53911409556f89cc894c6a5
David <dav2724@gmail.com>
2015-11-10 23:22:03 -0500

    edited index page

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,10 @@
 class Project < ActiveRecord::Base
   has_and_belongs_to_many :products
   belongs_to :user
+
   has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  
   geocoded_by :address
   after_validation :geocode
 end
